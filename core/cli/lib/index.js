fd56e38992d2fcacc96fbdd2addf5ff6163e1b3f
jeff9511 <870355761@qq.com>
2022-01-24 22:40:49 +0800

    feat：检查 node 版本开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,10 +3,32 @@
 module.exports = core
 
 const log = require('@jeff9511-cli/log')
-
 const pkg = require('../package.json')
+const constant = require('./const')
+const semver = require('semver')
+const colors = require('colors/safe')
+
 function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (error) {
+    log.error(error.message)
+  }
+}
+
+function checkNodeVersion() {
+  // 获取当前 node 版本号
+  const currentVersion = process.version
+
+  // 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION
+
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(
+      colors.red(`jeff-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`)
+    )
+  }
 }
 
 function checkPkgVersion() {
